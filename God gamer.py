96349e8cab907f2f694bace032a55861f8c4f9b1
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-02-22 10:33:31 +0000

    b

--- God gamer.py
+++ God gamer.py
@@ -1,1 +1,11 @@
-import turtle
+import time
+from turtle import Turtle
+laura = Turtle()
+rik = Turtle()
+lauren = Turtle()
+carrieanne = Turtle()
+laura.color('red')
+laura.shape('turtle')
+laura.penup()
+laura.goto(-160, 100)
+laura.pendown()
