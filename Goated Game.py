8937b26de557cd971165dbfe381a95a459ad7c54
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-03-03 10:12:50 +0000

    i

--- Goated Game.py
+++ Goated Game.py
@@ -3,4 +3,8 @@ from time import sleep
 from shapes import Paper, Rectangle, Oval, Triangle
 from room import Room
 
-kitchen = Room('kitchen')
+kitchen = Room('Kitchen')
+kitchen.set_description("A dank and dirty room buzzing with flies")
+kitchen.get_description()
+ballroom = Room('Ballroom')
+dining_hall = Room('Dining Hall')
