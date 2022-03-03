e65557115444bdc6f8cacda104435f4cdbd2449f
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-03-03 10:37:42 +0000

    k

--- Goated Game.py
+++ Goated Game.py
@@ -3,16 +3,24 @@ from time import sleep
 from shapes import Paper, Rectangle, Oval, Triangle
 from room import Room
 
-kitchen = Room('Kitchen')
+kitchen = Room('kitchen')
 kitchen.set_description("A dank and dirty room buzzing with flies")
 
-ballroom = Room('Ballroom')
+ballroom = Room('ballroom')
 ballroom.set_description("A vast room with a shiny wooden floor; huge candlesticks guard the entrance")
 
-dining_hall = Room('Dining Hall')
+dining_hall = Room('dining hall')
 dining_hall.set_description("A large room with ornate golden decorations on each wall")
 
 kitchen.link_room(dining_hall, 'south')
 dining_hall.link_room(kitchen, 'north')
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
 
-dining_hall.get_details()
+current_room = kitchen
+
+while True:
+    print('\n')
+    current_room.get_details()
+    command=input('> ')
+    current_room = current_room.move(command)
