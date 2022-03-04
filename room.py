484ea0fbfd11e8cecdb357e89d6e183a09c31dd1
ComputingGod <gibbard.o@oundleschool.org.uk>
2022-03-04 12:52:25 +0000

    n

--- room.py
+++ room.py
@@ -3,18 +3,25 @@ class Room():
         self.name = room_name
         self.description = None
         self.linked_rooms = {}
+        self.character = None
+
+    def set_description(self, room_description):
+        self.description = room_description
+
+    def get_description(self):
+        return self.description
 
     def set_name(self, room_name):
         self.name = room_name
 
     def get_name(self):
         return self.name
-    
-    def set_description(self, room_description):
-        self.description = room_description
 
-    def get_description(self):
-        return self.description
+    def set_character(self, new_character):
+        self.character = new_character
+
+    def get_character(self):
+        return self.character
     
     def describe(self):
         print(self.description)
